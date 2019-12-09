114102040f6de8e90edf0929bb4a08eefe1c39f4
Blaine Mulligan <blaine.mulligan@gmail.com>
2019-12-09 16:21:43 -0600

    12/9/2019 - 2

--- Week1/lesson1.py
+++ Week1/lesson1.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
+
+my_str = 'whatever'
+my_longstr = '''this is a
+multiline
+string
+'''
+
+print(my_longstr)
 
 try:
     ip_addr1 = raw_input("Enter an IP address: ")
